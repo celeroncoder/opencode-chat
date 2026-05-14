a441cf9f2ffe9c1031a77e3a9bc022374465c3bb
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2026-05-14 15:38:46 +0530

    feat: enhance scrollbar styling and update layout structure for improved rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,7 @@ export default async function RootLayout({
         figtreeHeading.variable,
       )}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <ServiceWorkerRegister />
         <ClerkProvider>
           <ThemeProvider
@@ -93,9 +93,9 @@ export default async function RootLayout({
                     }))}
                   />
                   <SidebarInset>
-                    <main className="flex-1 flex flex-col min-h-0">
+                    <div className="flex-1 flex flex-col min-h-0">
                       {children}
-                    </main>
+                    </div>
                   </SidebarInset>
                 </SidebarProvider>
               </Show>
